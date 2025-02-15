b93340fc67346bfbc0a3608818e2a6c46473b460
Madhumithaa-R-M7 <madhumithaarm7@gmail.com>
2025-02-15 17:25:30 +0530

    Initial commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,12 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+  function App() {
+    let name="Madhumithaa R M"
+    const rStyle={
+      textAlign:"center"
+    }
+    alert("Excuse meee Ba-bo!")
 
-  return (
-    <h1>Madhumithaa R M</h1>
-  )
-}
+    const Min={
+      name:"Madhumithaa R M",
+      age:18
+    }
+    console.log('My age is ',+Min.age)
+   return(
+    <div className='jeon'>
+      <h2>Name:{Min.name}</h2>
+      <h2 className='jay'>My age is {+Min.age}</h2>
+    </div>
+   )
 
+  }
 export default App
